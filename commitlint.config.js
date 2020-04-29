83ae2c481b3fe8ec41d2ad7d1b60d7d1f9abf4e5
胡江 <17600112740@163.com>
2020-04-29 21:01:11 +0800

    sha dong xi a

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', [
-            "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert", "upgrade", "revert", "build"
-        ]],
-        'subject-full-stop': [0, 'never'],
-        'subject-case': [0, 'never']
-    }
-}
+module.exports = {extends: ['@commitlint/config-conventional']}
